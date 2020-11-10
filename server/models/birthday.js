2e193911eb0ff6b8d78f0ad9da960e986b3cc453
christopher garcia <garcia.christopher1986@gmail.com>
2020-11-10 16:37:22 -0500

    sorta

---
+++ server/models/birthday.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const birthdaySchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    cohort_number: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    }
+
+});
+
+const Birthday = mongoose.model('Birthday', birthdaySchema);
+
+module.exports = Birthday;
--- server/routes/birthdays.js
+++ server/routes/birthdays.js
@@ -1,6 +1,54 @@
-const router = require('express').Router(),
-    { Birthday } = require('server/models/birthday.model.js')
+const router = require('express').Router();
+const Birthday = require('../db/models/birthday');
 
-    router.post('/', Birthday)
 
-module.exports = router
+router.get('/birthdays', async (req,res) => {
+    try {
+        const birthdays = await Birthday.find();
+        res.json(birthdays);
+        } catch (err) {
+            console.log(err.toString());
+        }
+});
+router.post('/birthdays', async (req, res) => {
+    try {
+        const birthday = new Birthday(req.body);
+        const response = await birthday.save();
+        res.json(response);
+        } catch (err) {
+            console.log(err.toString());
+        }
+});
+router.get('/birthday/:id', async (req, res) => {
+    try {
+        const birthday = await Birthday.findById(req.params.id)
+        res.jason(birthday);
+    } catch (err) {
+        console.log(err.toString());
+    }
+});
+
+router.put('/birthday/:id', async (req, res) => {
+    try {
+        const birthday = await Birthday.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        await birthday.save()
+        res.jason(birthday);
+    } catch (err) {
+        console.log(err.toString());
+    }
+});
+
+router.delete('/birthday/:id', async (req, res) =>{
+    try {
+        await Birthday.findByIdAndDelete(req.params.id);
+        res.json('birthday deleted')
+    } catch (err) {
+        console.log(err.toString());
+    }
+}
+);
+
+module.exports = router;
